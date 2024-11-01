964b32b78ac285c453dc319691f7a0d65d00487c
roman_kobzarov <backend1974@gmail.com>
2024-11-01 19:40:04 +0200

    feat: add accessor and mutator for User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
@@ -55,5 +57,31 @@ class User extends Authenticatable
     public function position(): BelongsTo
     {
         return $this->belongsTo(Position::class);
+    }
+
+    /**
+     * Accessor and mutator for 'email'
+     *
+     * @return Attribute
+     */
+    protected function email(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => $value,
+            set: fn($value) => Str::lower(trim($value)),
+        );
+    }
+
+    /**
+     * Accessor and mutator for 'name'
+     *
+     * @return Attribute
+     */
+    protected function name(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => trim($value),
+            set: fn($value) => trim($value),
+        );
     }
 }
