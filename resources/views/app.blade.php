82e066f8ca43dc8bb5b498885e9025178d8acc17
roman_kobzarov <backend1974@gmail.com>
2024-11-11 19:51:55 +0200

    feat: add tailwind css

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full bg-white">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel + Vue App for abz.agency</title>
     @vite('resources/js/app.js')
 </head>
-<body>
+<body class="h-full">
 <div id="app"></div>
 </body>
 </html>
